bf7766b0f63a08916291592f2233e800b8b95136
Александр Немеров <nemerov_a@mail.ru>
2021-12-12 15:48:57 +0800

    Добавьте файлы проекта.

---
+++ PraW_4_Base_progs/Source.cpp
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#define N 10
+#define M 1000
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <conio.h>
+#include <time.h>
+#include <Windows.h>
+
+using namespace std;
+
+void main() {
+	setlocale(LC_ALL, "rus");
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	/*Написать функцию, которая возвращает наибольший общий делитель двух целых чисел.*/
+	
+
+}
